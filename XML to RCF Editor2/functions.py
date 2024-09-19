b8c7625a9989811bc50cfae857d053abe84afa82
Hassan <ext.hassan.lotfy@appraid-tech.com>
2024-09-19 14:05:51 +0300

    updated files after reviews
    updated

--- XML to RCF Editor2/functions.py
+++ XML to RCF Editor2/functions.py
@@ -1,6 +1,25 @@
 import xml.etree.ElementTree as ET
 import copy
 import json
+import argparse
+
+"""
+function name: handle_arguments
+function description: handling parsed arguments from terminal
+inputs: None
+outputs: arguments
+"""
+def handle_arguments():
+    parser = argparse.ArgumentParser(description="Script to edit xml files and save it as .rcf")
+
+    # create arguments
+    parser.add_argument('input_xml_file_name', help="input xml file name", type=str)
+    parser.add_argument('input_json_file_name', help="input xml file name", type=str)
+    parser.add_argument('output_rcf_file_name', help="output rcf file name", type=str)
+
+    # load arguments into parse_arguments list
+    return parser.parse_args()
+
 
 """
 function name: parse_xml_return_root
@@ -17,11 +36,8 @@ def parse_xml_return_root(file_name_with_path):
         tree = ET.parse(rf"{file_name_with_path}")
         root = tree.getroot()
         return root
-    except FileNotFoundError:
-        print("File not found")
-        return None
-    except ET.ParseError:
-        print("The file could not be parsed. It may be malformed or contain invalid XML")
+    except:
+        print("File directory might be incorrect, or file content is not valid")
         return None
 
 
@@ -38,13 +54,13 @@ def parse_json_return_dict(file_name_with_path):
     try:
         # parse the json file
         with open(file_name_with_path, 'r') as file:
-            json_dictionary = json.load(file)
-            return json_dictionary
+            try:
+                json_dictionary = json.load(file)
+                return json_dictionary
+            except json.JSONDecodeError:
+                print("The file could not be parsed. It may be malformed or contain invalid json")
     except FileNotFoundError:
         print("File not found")
-        return None
-    except json.JSONDecodeError:
-        print("The file could not be parsed. It may be malformed or contain invalid json")
         return None
 
 
@@ -71,7 +87,7 @@ def loop_and_enable(element, json_dictionary):
 
                 ## if found in the dictionary, get the messages IDs to be edited
                 # activate this rule then enable messages
-                element.set('active', 'yes')
+                sub.set('active', 'yes')
                 messages_id = json_dictionary[rule_id]
                 enable_messages(sub,messages_id)
 
@@ -125,51 +141,7 @@ def enable_messages(element, messages_key_val, tag = "enforcement" ,message_key 
 
 
 
-"""
-function name: find_and_edit
-function description: identifies message address using attribute and its value (ex: 'id'='0304'). searches for parent first
-using any parent att and its value (ex: id = 'Rule-11.1'). If not found returns error flag, otherwise it sets active to yes then
-searches inside for specific tag (tag) (ex: enforcement). If not found returns error flag, otherwise it searches for tag 
-using one of its attributes and its value (ex: id= '0302'). if found, edit att and value (req_att_name and req_att_val), if not
-found, create a new tag line with the required attribute value.
-inputs: xml_file, parent_att_name, parent_att_val, tag ,tag_att_name, tag_att_val, req_att_name, req_att_val
-return: True, False (in case parent or tag not found)
-"""
 
-def find_and_edit(xml_file, parent_att_name, parent_att_val, tag ,tag_att_name, tag_att_val, req_att_name, req_att_val):
-    # search for parent using one of its attributes (id for example)
-    parent = xml_file.find(f".//*[@{parent_att_name}='{parent_att_val}']")
-
-    # check if it exists or not
-    if parent is None: # if not found return error flag
-        print("parent 1 not found")
-        return False
-    else: # if found, search for the next parent inside
-        parent.set('active', 'yes')
-        parent2 = parent.find(f'{tag}')
-        # check if next parent exists or not
-        if parent2 is None: # if not found return error flag
-            print("parent 2 not found")
-            return False
-        else: # if found, search for the message using one if its attributes (id for example)
-            tag = parent.find(f".//*[@{tag_att_name}='{tag_att_val}']")
-            # check if the message exists or not
-            if tag is not None: # if found, change given attribute to required value
-                tag.set(req_att_name, req_att_val)
-                print("message found and modified")
-            else: # if not found, add it
-                # copy the first message
-                first_message = parent2[0] if len(parent2) > 0 else None
-                # get a clone
-                new_message = copy.deepcopy(first_message)
-                # edit the clone attributes: set id, then set the required attribute
-                new_message.set(tag_att_name, tag_att_val)
-                new_message.set(req_att_name, req_att_val)
-                # append the clone to the XML file
-                parent2.append(new_message)
-                print("message does not exist. message added")
-
-            return True
 
 
 """
@@ -183,7 +155,6 @@ outputs: None (creates .rcf file but no return)
 def update_into_rcf(root, file_name_with_path):
     tree = ET.ElementTree(root)
 
-    ###### generate rcf #########
     # Ensure extension is .xml
     if file_name_with_path.endswith('.rcf'):  pass
     else:   file_name_with_path += ".rcf"
@@ -196,12 +167,4 @@ def update_into_rcf(root, file_name_with_path):
     except:
         print("incorrect output directory")
 
-    ####### generate xml (optional for testing) ########
-    try:
-        file_name_with_path = file_name_with_path.replace(".rcf", ".xml")
-        # if "edited_xml_file.xml" exists, it will be overwritten
-        with open(rf"{file_name_with_path}", "wb") as file:  # wb write binary mode, necessary when operating xml using python
-            tree.write(file, encoding='utf-8', xml_declaration=False)
-    except:
-        print("incorrect output directory")
 
